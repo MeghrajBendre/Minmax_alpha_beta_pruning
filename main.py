4bc49af3c0116b0f91017ded70557b86093732b3
Meghraj Bendre <bendremeghraj@gmail.com>
2018-01-27 20:31:47 -0800

    ROOT node creation

--- main.py
+++ main.py
@@ -5,6 +5,29 @@
 Usage: python main.py
 """
 
+#simple class for node manipulation
+class Node:
+    def __init__(self, player, state, parent=None):
+        self.player = player
+        self.state = state
+        self.parent = parent    #keeps track of the parent
+        self.children = []      #keeps track of the children for traversal
+
+    def tree_creation(self, player, initial_state, parent):
+
+
+
+#class to construct the game tree 
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def tree_creation(self, player, initial_state, parent):
+        if self.root:
+            return self.root.tree_creation(player, initial_state, parent)
+        else:
+            self.root = Node(player, initial_state, parent) 
+
 #reads input file and does necessary formatting
 def read_input_file():
     input_file = open("input.txt")
@@ -36,6 +59,8 @@ def read_input_file():
 #main function to handle user interface
 def main():
     player,initial_state = read_input_file()
+    game_tree = Tree()
+    game_tree.tree_creation(player, initial_state, parent)
 
 #call to main function
 if __name__ == '__main__':
